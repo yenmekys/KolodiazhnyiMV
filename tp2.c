6b357d8df5dc4401cf6d6822b5c40da383919f30
yenmekys <mkolodyazhnyy07@gmail.com>
2024-09-24 22:55:12 +0300

    пункт 1.3

---
+++ tp2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main(void) {
+    int a, b;
+    printf("Введите число a ");
+    scanf("%d", &a);
+    printf("Введите число b ");
+    scanf("%d", &b);
+    return 0;
+}
