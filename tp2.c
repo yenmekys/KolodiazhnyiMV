f830d7c45e95d3871615f86d07c29e31de0360c1
yenmekys <mkolodyazhnyy07@gmail.com>
2024-09-24 22:47:10 +0300

    пункт 1.2

---
+++ tp2.c
@@ -0,0 +1,12 @@
+
+
+int main(void) {
+    int a, b;
+    printf("Введите число a ");
+    scanf("%d", &a);
+    printf("Введите число b ");
+    scanf("%d", &b);
+
+    printf("%d\n", a - b);
+    return 0; 
+}
